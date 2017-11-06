441ab7864b729adbd0e3810f9a6c11650cd71793
Sjoerd Adema <sjoerd@triz.nu>
2017-11-06 13:01:30 +0100

    Problem 2 complete

--- problem2.py
+++ problem2.py
@@ -30,5 +30,30 @@ for step in steps:
 
     code += str(keypad[location[1]][location[0]])
 
-print(code)
+print('Code normal: ', code)
 
+keypad = [
+    [None, None, 1, None, None],
+    [None, 2, 3, 4, None],
+    [5, 6, 7, 8, 9],
+    [None, 'A', 'B', 'C', None],
+    [None, None, 'D', None, None]
+]
+
+code = ''
+location = (0, 2)
+for step in steps:
+    for move in step:
+        try:
+            x = location[0] + directions[move][0]
+            y = location[1] + directions[move][1]
+
+            if 0 <= y <= 4 and 0 <= x <= 4 and keypad[y][x] is not None:
+                location = (x, y)
+
+        except KeyError:
+            continue
+
+    code += str(keypad[location[1]][location[0]])
+
+print('Code not normal: ', code)
