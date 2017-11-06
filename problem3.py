4ffc93cf10713a0acef2a71c728b92b81a121525
Sjoerd Adema <sjoerd@triz.nu>
2017-11-06 13:14:26 +0100

    Problem 3 complete

--- problem3.py
+++ problem3.py
@@ -12,5 +12,18 @@ for triangle in triangles:
     if sides[0] + sides[1] > sides[2]:
         valid += 1
 
-print(valid)
+print('Horizontal number of triangles: ', valid)
 
+vertical_triangles = []
+for index in range(0, len(triangles), 3):
+    vertical_triangles.append((triangles[index][0], triangles[index + 1][0], triangles[index + 2][0]))
+    vertical_triangles.append((triangles[index][1], triangles[index + 1][1], triangles[index + 2][1]))
+    vertical_triangles.append((triangles[index][2], triangles[index + 1][2], triangles[index + 2][2]))
+
+valid = 0
+for triangle in vertical_triangles:
+    sides = sorted(triangle)
+    if sides[0] + sides[1] > sides[2]:
+        valid += 1
+
+print('Vertical number of triangles: ', valid)
