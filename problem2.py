ef5132949a1179996cb6f5194a4c6c37d3b67b1b
Sjoerd Adema <sjoerd@triz.nu>
2017-12-03 17:57:56 +0100

    Problem 2 cleaned up

--- problem2.py
+++ problem2.py
@@ -1,28 +1,18 @@
-content = None
 with open('assets/problem2.txt', 'r') as file:
-    content = file.readlines()
-
-rows = []
-for line in content:
-    row = line.replace('\n', '').split('\t')
-    for i in range(len(row)):
-        row[i] = int(row[i])
-
-    rows.append(row)
+    rows = [sorted(list(map(int, row.split('\t')))) for row in file.readlines()]
 
 checksum = []
 for row in rows:
-    sorted_row = sorted(row)
-    checksum.append(abs(sorted_row[0] - sorted_row[-1]))
+    checksum.append(abs(row[0] - row[-1]))
 
 print('Checksum rows: ', sum(checksum))
 
 checksum = []
 for row in rows:
-    sorted_row = sorted(row)[::-1]
-    for i in range(len(sorted_row)):
-        for j in range(i + 1, len(sorted_row)):
-            if sorted_row[i] % sorted_row[j] == 0:
-                checksum.append(int(sorted_row[i] / sorted_row[j]))
+    row = row[::-1]
+    for i in range(len(row)):
+        for j in range(i + 1, len(row)):
+            if row[i] % row[j] == 0:
+                checksum.append(int(row[i] / row[j]))
 
 print('Checksum divisors: ', sum(checksum))
