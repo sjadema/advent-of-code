c2858a3d181aa72d9fa8612c4e215e940a7efd01
Sjoerd Adema <sjoerd@triz.nu>
2017-12-03 17:36:29 +0100

    Problem 1 cleaned up

--- problem1.py
+++ problem1.py
@@ -1,18 +1,12 @@
-content = None
 with open('assets/problem1.txt', 'r') as file:
-    content = file.read().strip()
-
-chars = []
-for char in content:
-    chars.append(char)
+    chars = [int(char) for char in file.read().strip()]
 
 length = len(chars)
-neighbouring_chars = chars[1:] + chars[0:1]
 
 total = 0
-for i in range(length - 1):
-    if neighbouring_chars[i] == neighbouring_chars[i + 1]:
-        total += int(neighbouring_chars[i])
+for i in range(length):
+    if chars[i] == chars[(i + 1) % length]:
+        total += int(chars[i])
 
 print('Total neighbouring: ', total)
 
