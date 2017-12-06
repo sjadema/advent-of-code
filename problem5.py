188f558c93afaf9d9a73ff96e48a499b16e7c104
Sjoerd Adema <sjoerd@triz.nu>
2017-12-06 01:18:14 +0100

    Problem 5 complete

--- problem5.py
+++ problem5.py
@@ -1,4 +1,31 @@
 with open('assets/problem5.txt', 'r') as file:
     instructions = [int(row.strip()) for row in file.readlines()]
 
-print(instructions)
+jumps = list(instructions)
+steps = position = 0
+while position < len(jumps):
+    jumps[position] += 1
+    position += jumps[position] - 1
+    steps += 1
+
+print('Total steps: ', steps)
+
+jumps = list(instructions)
+steps = position = 0
+offset = jumps[0]
+while position < len(jumps):
+    if offset > 2:
+        jumps[position] -= 1
+        position += jumps[position] + 1
+    else:
+        jumps[position] += 1
+        position += jumps[position] - 1
+
+    steps += 1
+
+    try:
+        offset = jumps[position]
+    except IndexError:
+        break
+
+print('Total steps: ', steps)
