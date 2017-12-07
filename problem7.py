b700b1b112fae81714ce539fd79bc6f0701cbf2f
Sjoerd Adema <sjoerd@triz.nu>
2017-12-07 23:25:54 +0100

    Problem 4 complete

--- problem7.py
+++ problem7.py
@@ -6,22 +6,6 @@ from util.node import Node
 with open('assets/problem7.txt', 'r') as file:
     rows = [row.strip() for row in file.readlines()]
 
-# rows = [
-#     'pbga (66)',
-#     'xhth (57)',
-#     'ebii (61)',
-#     'havc (66)',
-#     'ktlj (57)',
-#     'fwft (72) -> ktlj, cntj, xhth',
-#     'qoyq (66)',
-#     'padx (45) -> pbga, havc, qoyq',
-#     'tknk (41) -> ugml, padx, fwft',
-#     'jptl (61)',
-#     'ugml (68) -> gyxo, ebii, jptl',
-#     'gyxo (61)',
-#     'cntj (57)',
-# ]
-
 tree = {}
 for row in rows:
     match = re.search('^(?P<name>[^\s]+)\s?\((?P<weight>[\d]+)\)', row)
