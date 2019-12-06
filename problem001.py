4990abe732d81de8352abda452aa4c1a3a511ea1
Sjoerd Adema <sjoerd@triz.nu>
2019-12-06 01:23:09 +0100

    Problem 1 cleaner

--- problem001.py
+++ problem001.py
@@ -1,24 +1,21 @@
-import math
-
 with open('assets/problem001.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
-fuel = []
+fuels = []
 for line in lines:
-    fuel.append(math.floor(int(line) / 3) - 2)
+    fuels.append(int(line) // 3 - 2)
 
-print('Fuel required is {}.'.format(sum(fuel)))
+print('Fuel required is {}.'.format(sum(fuels)))
 
 def calculate_fuel(mass: int) -> int:
-    fuel = max(math.floor(mass / 3) - 2, 0)
+    fuel = max(mass // 3 - 2, 0)
     if fuel > 0:
         fuel += calculate_fuel(fuel)
 
     return fuel
 
-fuel = []
+fuels = []
 for line in lines:
-    fuel.append(calculate_fuel(int(line)))
+    fuels.append(calculate_fuel(int(line)))
 
-print('Adjusted fuel required is {}.'.format(sum(fuel)))
-
+print('Adjusted fuel required is {}.'.format(sum(fuels)))
