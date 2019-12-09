5c2235145bb650df3997f6eed8ecca10b9f7d8b7
Sjoerd Adema <sjoerd@triz.nu>
2019-12-09 02:31:04 +0100

    Problem 6 with node

--- problem006.py
+++ problem006.py
@@ -1,34 +1,32 @@
+from util.node import Node
+
 with open('assets/problem006.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
-orbit_map = {}
+nodes = {}
 for line in lines:
-    objects = line.split(')')
+    masses = line.split(')')
 
-    try:
-        orbit_map[objects[0]].append(objects[1])
-    except KeyError:
-        orbit_map[objects[0]] = [objects[1]]
-
-orbiting = []
-for value in orbit_map.values():
-    orbiting += value
-
-center = set(orbit_map.keys()).difference(set(orbiting)).pop()
-
-total_orbits = {}
-
-
-def determine_orbits(start: str, orbits: int = 0) -> None:
-    total_orbits[start] = orbits
-
-    masses = orbit_map[start]
     for mass in masses:
-        if mass in orbit_map.keys():
-            determine_orbits(mass, orbits + 1)
-        else:
-            total_orbits[mass] = orbits + 1
+        if mass not in nodes:
+            nodes[mass] = Node(mass)
 
+for line in lines:
+    masses = line.split(')')
 
-determine_orbits(center)
-print('Total number of orbits: {}.'.format(sum(total_orbits.values())))
+    nodes[masses[0]].add_child(nodes[masses[1]])
+
+total_depth = 0
+for node in nodes.values():
+    total_depth += node.depth()
+
+print('Total number of orbits: {}.'.format(total_depth))
+
+ancestors_you = set([node.value for node in nodes['YOU'].get_ancestors()])
+ancestors_san = set([node.value for node in nodes['SAN'].get_ancestors()])
+ancestors_common = ancestors_you.intersection(ancestors_san)
+
+difference_you = ancestors_you.difference(ancestors_common)
+difference_san = ancestors_san.difference(ancestors_common)
+
+print('Total number of transfers: {}.'.format(len(difference_you) + len(difference_san)))
