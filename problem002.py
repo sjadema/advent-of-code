0c1517d266ec85bf9cf264c7599ee521edc9336b
Sjoerd Adema <sjoerd@triz.nu>
2020-12-03 00:26:53 +0100

    Problem 2, part 2

--- problem002.py
+++ problem002.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 with open('assets/problem002.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
@@ -11,11 +13,18 @@ for line in lines:
         'password': parts[1].strip()
     })
 
-valid = 0
+range_valid = 0
+position_valid = 0
 for password in passwords:
     boundaries = [int(boundary) for boundary in password['range'].split('-')]
     delta = len(password['password']) - len(password['password'].replace(password['char'], ''))
     if boundaries[0] <= delta <= boundaries[1]:
-        valid += 1
+        range_valid += 1
 
-print('Valid passwords: {}'.format(valid))
+    positions = boundaries
+    delta = [password['password'][position - 1] == password['char'] for position in positions]
+    if bool(reduce(lambda x, y: x ^ y, delta)):
+        position_valid += 1
+
+print('Valid range passwords: {}.'.format(range_valid))
+print('Valid position passwords: {}.'.format(position_valid))
