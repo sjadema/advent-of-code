5324b941d1f549a09a94d30ee01fdff9a4d1d33a
Sjoerd Adema <sjoerd@triz.nu>
2020-12-02 23:26:03 +0100

    Problem 1, part 2

--- problem001.py
+++ problem001.py
@@ -5,5 +5,8 @@ expenses = [int(expense) for expense in lines]
 for i in range(0, len(expenses)):
     for j in range(i + 1, len(expenses)):
         if 2020 == expenses[i] + expenses[j]:
-            print('Product is {}.'.format(expenses[i] * expenses[j]))
-            break
+            print('Double product is {}.'.format(expenses[i] * expenses[j]))
+
+        for k in range(j + 1, len(expenses)):
+            if 2020 == expenses[i] + expenses[j] + expenses[k]:
+                print('Triple product is {}.'.format(expenses[i] * expenses[j] * expenses[k]))
