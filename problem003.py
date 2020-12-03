8a0cece583632ba5bf01181f83ea6b19cf7ae229
Sjoerd Adema <sjoerd@triz.nu>
2020-12-03 22:49:37 +0100

    Problem 3, part 2

--- problem003.py
+++ problem003.py
@@ -3,25 +3,44 @@ import math
 with open('assets/problem003.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
-fraction = 3 / 1
-multiplier = math.ceil(len(lines) * fraction / len(lines[0]))
 
-grid = []
-for y in range(0, len(lines)):
-    grid.append([])
-    for x in (lines[y] * multiplier):
-        grid[y].append(x)
+def calculate_trees(right: int, down: int) -> int:
+    fraction = right / down
+    multiplier = math.ceil(len(lines) * fraction / len(lines[0]))
 
-x = y = 0
-trees = 0
-while True:
-    y += 1
-    x += 3
+    grid = []
+    for y in range(0, len(lines)):
+        grid.append([])
+        for x in (lines[y] * multiplier):
+            grid[y].append(x)
 
-    try:
-        if '#' == grid[y][x]:
-            trees += 1
-    except IndexError:
-        break
+    x = y = 0
+    trees = 0
+    while True:
+        y += down
+        x += right
 
-print('Number of trees: {}.'.format(trees))
+        try:
+            if '#' == grid[y][x]:
+                trees += 1
+        except IndexError:
+            break
+
+    return trees
+
+
+print('Number of trees: {}.'.format(calculate_trees(3, 1)))
+
+steps = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+]
+
+for i in range(0, len(steps)):
+    step = steps[i]
+    steps[i] = calculate_trees(step[0], step[1])
+
+print('Total number of trees: {}.'.format(math.prod(steps)))
