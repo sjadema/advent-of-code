6afa6e5626055b6d469cf50c3776783dba504aae
Sjoerd Adema <sjoerd@triz.nu>
2020-12-05 14:14:47 +0100

    Problem 4, part 2

--- problem004.py
+++ problem004.py
@@ -1,3 +1,5 @@
+import re
+
 with open('assets/problem004.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
@@ -23,5 +25,24 @@ valid_passports = 0
 for passport in passports:
     if 8 == len(passport) or (7 == len(passport) and 'cid' not in passport):
         valid_passports += 1
+
+print('Number of valid passports: {}.'.format(valid_passports))
+
+valid_passports = 0
+for passport in passports:
+    birth_year = int(passport['byr']) if 'byr' in passport else 0
+    issue_year = int(passport['iyr']) if 'iyr' in passport else 0
+    expiration_year = int(passport['eyr']) if 'eyr' in passport else 0
+    height = passport['hgt'] if 'hgt' in passport else '0cm'
+    hair_color = passport['hcl'] if 'hcl' in passport else ''
+    eye_color = passport['ecl'] if 'ecl' in passport else ''
+    passport_id = passport['pid'] if 'pid' in passport else ''
+
+    if 1920 <= birth_year <= 2002 and 2010 <= issue_year <= 2020 <= expiration_year <= 2030:
+        if eye_color in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'] and re.compile('^#[0-9a-f]{6}$').match(hair_color):
+            if 9 == len(passport_id):
+                real_height = int(height.replace('cm', '').replace('in', ''))
+                if ('cm' in height and 150 <= real_height <= 193) or ('in' in height and 59 <= real_height <= 76):
+                    valid_passports += 1
 
 print('Number of valid passports: {}.'.format(valid_passports))
