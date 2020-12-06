db13ecbf3f053cab0483b9e078e181c8fcc4e2be
Sjoerd Adema <sjoerd@triz.nu>
2020-12-06 17:08:15 +0100

    Problem 6, part 1

--- problem006.py
+++ problem006.py
@@ -1,20 +1,20 @@
+from functools import reduce
+
 with open('assets/problem006.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
-groups = ['']
+groups = [[]]
 i = 0
 for line in lines:
     if '' == line:
         i += 1
-        groups.append('')
+        groups.append([])
+        continue
 
-    groups[i] += line
+    groups[i].append(line)
 
+anyone = []
 for i in range(0, len(groups)):
-    groups[i] = set(list(groups[i]))
+    anyone.append(set(list(''.join(groups[i]))))
 
-total = 0
-for group in groups:
-    total += len(group)
-
-print('Sum of all answered questions: {}.'.format(total))
+print('Sum of all answered questions: {}.'.format(reduce(lambda total, group: total + len(group), anyone, 0)))
