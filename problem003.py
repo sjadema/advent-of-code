b4a952bd91a3567d3eaacd9ae1a51838b979d738
Sjoerd Adema <sjoerd@triz.nu>
2020-12-06 22:32:03 +0100

    Problem 3 reduce

--- problem003.py
+++ problem003.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 with open('assets/problem003.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
@@ -7,8 +9,7 @@ def calculate_trees(right: int, down: int) -> int:
     y = down
     trees = 0
     while y < len(lines):
-        if '#' == lines[y][x % len(lines[y])]:
-            trees += 1
+        trees += int('#' == lines[y][x % len(lines[y])])
 
         y += down
         x += right
@@ -26,9 +27,4 @@ steps = [
     [1, 2],
 ]
 
-total_trees = 1
-for i in range(0, len(steps)):
-    step = steps[i]
-    total_trees *= calculate_trees(step[0], step[1])
-
-print('Total number of trees: {}.'.format(total_trees))
+print('Total number of trees: {}.'.format(reduce(lambda t, s: t * calculate_trees(s[0], s[1]), steps, 1)))
