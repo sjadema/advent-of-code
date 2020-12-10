ee8854e5bcb9f0bef7e4e90adb6d3882b18685f0
Sjoerd Adema <sjoerd@triz.nu>
2020-12-10 19:45:17 +0100

    Problem 7, part 2

--- problem007.py
+++ problem007.py
@@ -24,3 +24,29 @@ for name in visit:
 
 visit.remove(target)
 print('Total number of bags containing at least one {:s} bag: {:d}.'.format(target, len(visit)))
+
+visit = [target]
+bag_weights = {}
+for name in visit:
+    bag_weights[name] = {'bags': [], 'weight': 1}
+    for inner_bag_name in bags[name]:
+        bag_weights[name]['bags'] += [inner_bag_name] * bags[name][inner_bag_name]
+        visit.append(inner_bag_name)
+
+
+visit = list(bag_weights.keys())
+while len(visit):
+    for name in visit:
+        bag = bag_weights[name]
+        if 0 == len(bag['bags']):
+            visit.remove(name)
+
+            for check_bag in bag_weights.values():
+                if name in check_bag['bags']:
+                    total_bags = check_bag['bags'].count(name)
+                    check_bag['weight'] += total_bags * bag['weight']
+
+                    while name in check_bag['bags']:
+                        check_bag['bags'].remove(name)
+
+print('Total number of bags contained in a {:s} bag: {:d}.'.format(target, bag_weights[target]['weight'] - 1))
