479dc67d9b5427237346c5746aeedb343bd9dd37
Sjoerd Adema <sjoerd@gitlabhost.com>
2021-12-01 18:19:48 +0100

    Problem 1

--- problem001.py
+++ problem001.py
@@ -1,12 +1,26 @@
 with open('assets/problem001.txt', 'r') as file:
-    lines = [line for line in file.read().splitlines()]
+    depths = [int(line) for line in file.read().splitlines()]
 
-expenses = [int(expense) for expense in lines]
-for i in range(0, len(expenses)):
-    for j in range(i + 1, len(expenses)):
-        if 2020 == expenses[i] + expenses[j]:
-            print('Double product is {}.'.format(expenses[i] * expenses[j]))
+print(depths)
 
-        for k in range(j + 1, len(expenses)):
-            if 2020 == expenses[i] + expenses[j] + expenses[k]:
-                print('Triple product is {}.'.format(expenses[i] * expenses[j] * expenses[k]))
+increased = 0
+for i in range(1, len(depths)):
+    if depths[i] > depths[i - 1]:
+        increased += 1
+
+print("Depth increased: {:d} times.".format(increased))
+
+windows = []
+for i in range(0, len(depths) - 2):
+    window = 0
+    for j in range(0, 3):
+        window += depths[i + j]
+
+    windows.append(window)
+
+increased = 0
+for i in range(1, len(windows)):
+    if windows[i] > windows[i - 1]:
+        increased += 1
+
+print("Depth windows increased: {:d} times.".format(increased))
