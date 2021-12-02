0e09350dd051a0b17bf91dd72e8aa7f1e6a80e60
Sjoerd Adema <sjoerd@gitlabhost.com>
2021-12-02 14:54:43 +0100

    Problem 2 part II

--- problem002.py
+++ problem002.py
@@ -1,15 +1,18 @@
 with open('assets/problem002.txt', 'r') as file:
     legs = [line for line in file.read().splitlines()]
 
-x = y = 0
+x = y = aim = y_aim = 0
 for leg in legs:
     direction, amount = tuple(leg.split(' '))
     amount = int(amount)
 
     if 'forward' == direction:
         x += amount
+        y_aim += (amount * aim)
     else:
         modifier = 1 if 'down' == direction else -1
         y += (amount * modifier)
+        aim += (amount * modifier)
 
-print("Position reached: ({:d}, {:d}) with product {:d}.".format(x, y, x * y))
+print("Position reached without aim: ({:d}, {:d}) with product {:d}.".format(x, y, x * y))
+print("Position reached with aim: ({:d}, {:d}) with product {:d}.".format(x, y_aim, x * y_aim))
