a34b2869b5d22fc916c13220240de42edad8d392
Sjoerd Adema <sjoerd@triz.nu>
2021-12-04 00:36:26 +0100

    Problem 11, part 1

--- problem010.py
+++ problem010.py
@@ -1,11 +1,85 @@
+from util.graph import Vertex, Graph
+
 with open('assets/problem010.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
 jolts = [0] + sorted([int(line) for line in lines])
+jolts += [jolts[-1] + 3]
 
-differences = {1: 0, 3: 1}
+differences = dict(zip(range(1, 4), [0] * 3))
 
 for i in range(0, len(jolts) - 1):
     differences[jolts[i + 1] - jolts[i]] += 1
 
 print('Difference product: {}'.format(differences[1] * differences[3]))
+
+connections = 0
+stripped_jolts = jolts[1:len(jolts) - 1]
+
+# jolts = [0, 1, 4, 5, 6, 7]
+
+cursor = 0
+while jolts.index(cursor) < len(jolts) - 1:
+    window = list(range(cursor + 1, cursor + 4))
+    visible = jolts[jolts.index(cursor) + 1:jolts.index(cursor) + 4]
+    # print(window, visible)
+
+    existing = list(set(window).intersection(visible))
+    if 1 < len(existing):
+        connections += len(existing)
+
+    print(existing)
+
+    cursor = existing[0]
+
+print(connections + 1)
+# for i in range(0, len(jolts)):
+#     for j in range(i, i + 3):
+#
+#
+#
+#
+#     for test in [jolt + 3, jolt + 2, jolt + 1]:
+#         if test in jolts:
+
+
+
+
+
+
+#
+#
+# vertices = {}
+# for jolt in jolts:
+# # for jolt in jolts[::-1]:
+#     if jolt not in vertices:
+#         vertices[jolt] = Vertex(jolt)
+#
+#     vertex = vertices[jolt]
+#
+#     for test in [jolt + 1, jolt + 2, jolt + 3]:
+#         if test in jolts:
+#             if test not in vertices:
+#                 vertices[test] = Vertex(test)
+#
+#             vertex.add_edge(vertices[test])
+#
+# graph = Graph.create(list(vertices.values()))
+# connections = 0
+# for jolt in jolts:
+#
+#
+#
+#
+# jolts_length = len(jolts)
+# # connections_one = graph.count_all_paths(vertices[jolts[0]], vertices[jolts[-1]])
+# # connections_two = 1
+# print(jolts_length, jolts_length // 4)
+# connections_one = graph.count_all_paths(vertices[jolts[0]], vertices[jolts[jolts_length // 4]])
+# connections_two = graph.count_all_paths(vertices[jolts[jolts_length // 4]], vertices[jolts[jolts_length // 2]])
+# connections_three = graph.count_all_paths(vertices[jolts[jolts_length // 2]], vertices[jolts[jolts_length // 4 * 3]])
+# connections_four = graph.count_all_paths(vertices[jolts[jolts_length // 4 * 3]], vertices[jolts[-1]])
+#
+# print(connections_one, connections_two, connections_three, connections_four)
+#
+# print('Number of distinct connections: {}.'.format(connections_one * connections_two * connections_three * connections_four))
--- problem011.py
+++ problem011.py
@@ -10,29 +10,30 @@ for y in range(0, len(lines)):
     seats.append(row)
 
 
-def count_occupied(seats: list[list[str]], row: int, column: int) -> int:
-    occupied = 0
+def count_occupied_adjacent(seats: list[list[str]], row: int, column: int) -> int:
+    occupied_adjacent = 0
     for y in range(max(row - 1, 0), min(len(seats), row + 2)):
         for x in range(max(column - 1, 0), min(len(seats[row]), column + 2)):
             if y == row and x == column:
                 continue
 
-            occupied += int('#' == seats[y][x])
+            occupied_adjacent += int('#' == seats[y][x])
 
-    return occupied
+    return occupied_adjacent
 
 
+seats_adjacent = seats
 changes = -1
 while 0 != changes:
     changes = 0
     new_seats = []
 
-    for row in range(0, len(seats)):
+    for row in range(0, len(seats_adjacent)):
         new_row = []
-        for column in range(0, len(seats[row])):
-            seat = seats[row][column]
+        for column in range(0, len(seats_adjacent[row])):
+            seat = seats_adjacent[row][column]
 
-            occupied = count_occupied(seats, row, column)
+            occupied = count_occupied_adjacent(seats_adjacent, row, column)
             if 'L' == seat and 0 == occupied:
                 new_row.append('#')
                 changes += 1
@@ -44,6 +45,56 @@ while 0 != changes:
 
         new_seats.append(new_row)
 
-    seats = new_seats
+    seats_adjacent = new_seats
 
-print('Total number of occupied seats: {}.'.format(sum([row.count('#') for row in seats])))
+print('Total number of occupied seats: {}.'.format(sum([row.count('#') for row in seats_adjacent])))
+
+
+def count_occupied_seen(seats: list[list[str]], row: int, column: int) -> int:
+    occupied_seen = 0
+    for y in range(0, len(seats)):
+        delta = abs(y - row)
+        if 0 == delta:
+            columns = range(0, len(seats[row]))
+        else:
+            columns = range(max(column - delta, 0), min(len(seats[row]), column + delta + 1), delta)
+
+        for x in columns:
+            if y == row and x == column:
+                continue
+
+            if ()
+            occupied_seen =
+
+
+
+
+        for x in range()
+    return occupied_seen
+
+seats_seen = seats
+changes = -1
+while 0 != changes:
+    changes = 0
+    new_seats = []
+
+    for row in range(0, len(seats_seen)):
+        new_row = []
+        for column in range(0, len(seats_seen[row])):
+            seat = seats_seen[row][column]
+
+            occupied = count_occupied_adjacent(seats_seen, row, column)
+            if 'L' == seat and 0 == occupied:
+                new_row.append('#')
+                changes += 1
+            elif '#' == seat and 3 < occupied:
+                new_row.append('L')
+                changes += 1
+            else:
+                new_row.append(seat)
+
+        new_seats.append(new_row)
+
+    seats_adjacent = new_seats
+
+print('Total number of occupied seats: {}.'.format(sum([row.count('#') for row in seats_seen])))
