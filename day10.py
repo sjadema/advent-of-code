e83575fd51e9d9da637a92652d340352ef55ba1c
Sjoerd Adema <sjoerd@triz.nu>
2021-12-13 13:04:59 +0100

    Day 10, part I

--- day10.py
+++ day10.py
@@ -2,28 +2,24 @@ with open('assets/day10.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
 
-def _get_states() -> dict:
-    return {'(': ')', '[': ']', '{': '}', '<': '>'}
-
-
-def syntax_error_score(line: str) -> int:
-    states = _get_states()
-    scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
+def syntax_error(line: str) -> tuple:
+    pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}
 
     state = []
     for c in line:
-        if c in states:
-            state.append(states[c])
+        if c in pairs:
+            state.append(pairs[c])
         elif c != state[-1]:
-            return scores[c]
+            return 'corrupt', c, f"Expected {state[-1]}, but found {c} instead."
         else:
             state.pop()
 
-    return 0
+    return 'incomplete', ''.join(state), f"Complete by adding {''.join(reversed(state))}"
 
 
-score = 0
-for line in lines:
-    score += syntax_error_score(line)
+syntax_errors = [syntax_error(line) for line in lines]
 
-print(f"Syntax error score: {score}")
+corrupted = [syntax_error[1] for syntax_error in syntax_errors if 'corrupt' == syntax_error[0]]
+penalties = {')': 3, ']': 57, '}': 1197, '>': 25137}
+
+print(f"Corrupted syntax error score: {sum([penalties[c] for c in corrupted])}")
