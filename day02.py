5cf9491668b1dfa7e954573fb3c3ab3a60243780
Sjoerd Adema <sjoerd@gitlabhost.com>
2022-12-02 14:37:32 +0100

    Day 2, part II

--- day02.py
+++ day02.py
@@ -1,38 +1,39 @@
 with open('assets/day02.txt', 'r') as file:
     matches = [line for line in file.read().splitlines()]
 
-shapes = {'A': 1, 'B': 2, 'C': 3}
-shapes = shapes | dict(zip(['X', 'Y', 'Z'], shapes.values()))
+matches = [tuple(match.split(' ')) for match in matches]
+shapes = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+weights = {'person_a': ['C', 'A', 'B'], 'person_b': ['Z', 'X', 'Y']}
+
+
+def calculate_points(person_a: int, person_b: int) -> int:
+    if person_a == person_b:
+        return 3
+    elif (person_a + 1) % 3 == person_b:
+        return 6
+
+    return 0
+
 
 scores = []
 for match in matches:
-    person_a, person_b = tuple(match.split(' '))
-    score = {'shape': shapes[person_b]}
+    shape_a, shape_b = match
+    weight_a, weight_b = weights['person_a'].index(shape_a), weights['person_b'].index(shape_b)
 
-    weights = {'person_a': ['C', 'A', 'B'], 'person_b': ['Z', 'X', 'Y']}
-    weight_a, weight_b = weights['person_a'].index(person_a), weights['person_b'].index(person_b)
-
-    points = 0
-    if weight_a == weight_b:
-        points = 3
-    elif (weight_a + 1) % 3 == weight_b:
-        points = 6
-
-    score['points'] = points
-    scores.append(score)
+    scores.append({'score': shapes[shape_b], 'points': calculate_points(weight_a, weight_b)})
 
 print(f'''Total score: {sum([sum(score.values()) for score in scores])}''')
 
+weights['person_b'] = ['Y', 'Z', 'X']
 
-#
-# calories_per_elf = ' '.join(calories).split('  ')
-#
-# total_calories_per_elf = []
-# for elf in calories_per_elf:
-#     total_calories_per_elf.append(sum([int(calorie) for calorie in elf.split(' ')]))
-#
-# print(f'''Maximum total amount of calories: {max(total_calories_per_elf)}''')
-#
-# ordered_calories = sorted(total_calories_per_elf, reverse=True)
-#
-# print(f'''Top 3 of calories: {sum(ordered_calories[0:3])}''')
+scores = []
+for match in matches:
+    shape_a, shape_b = match
+    weight_a, weight_b = weights['person_a'].index(shape_a), weights['person_b'].index(shape_b)
+
+    shape_b = weights['person_a'][(weight_a + weight_b) % 3]
+    weight_b = weights['person_a'].index(shape_b)
+
+    scores.append({'score': shapes[shape_b], 'points': calculate_points(weight_a, weight_b)})
+
+print(f'''Total score: {sum([sum(score.values()) for score in scores])}''')
