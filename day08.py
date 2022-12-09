23b5735e94a99d3089689ab5f96fe6335ad01d4c
Sjoerd Adema <sjadema@gmail.com>
2022-12-09 02:38:34 +0100

    Day 8, part II without rotation

--- day08.py
+++ day08.py
@@ -4,53 +4,62 @@ with open('assets/day08.txt', 'r') as file:
 rows, columns = len(trees), len(trees[0])
 visible_trees, scenic_trees = [[False] * columns for _ in range(rows)], [[1] * columns for _ in range(rows)]
 
-for i in range(4):
-    # Rotate tree matrices 90 degrees clockwise
-    rotated_trees, rotated_visible_trees, rotated_scenic_trees = [], [], []
-    for x in range(columns):
-        trees_column, visible_trees_column, scenic_trees_column = [], [], []
-        for y in range(rows):
-            trees_column.append(trees[y][x])
-            visible_trees_column.append(visible_trees[y][x])
-            scenic_trees_column.append(scenic_trees[y][x])
+max_top = max_right = max_bottom = max_left = 0
+for y in range(rows):
+    for lower in range(columns):
+        upper = -1 - lower
 
-        rotated_trees.append(trees_column[::-1])
-        rotated_visible_trees.append(visible_trees_column[::-1])
-        rotated_scenic_trees.append(scenic_trees_column[::-1])
+        tree_top = trees[lower][y]
+        tree_right = trees[y][upper]
+        tree_bottom = trees[upper][y]
+        tree_left = trees[y][lower]
 
-    # Calculate tree visibility and scenic score
-    for y in range(rows):
-        max_height = 0
-        for x in range(columns):
-            tree = rotated_trees[y][x]
+        # Initialize borders
+        if lower == 0:
+            max_top, max_right, max_bottom, max_left = tree_top, tree_right, tree_bottom, tree_left
 
-            # First column
-            if x == 0:
-                max_height = tree
+            visible_trees[lower][y] = visible_trees[y][upper] = visible_trees[upper][y] = visible_trees[y][lower] = True
+            scenic_trees[lower][y] = scenic_trees[y][upper] = scenic_trees[upper][y] = scenic_trees[y][lower] = 0
 
-                rotated_visible_trees[y][x] = True
-                rotated_scenic_trees[y][x] = 0
+            continue
 
-                continue
+        # Visibility
+        if tree_top > max_top:
+            visible_trees[lower][y] = True
+            max_top = tree_top
 
-            # Visibility
-            if tree > max_height:
-                rotated_visible_trees[y][x] = True
-                max_height = tree
+        if tree_right > max_right:
+            visible_trees[y][upper] = True
+            max_right = tree_right
 
-            # View distance
-            view_distance = 0
-            for view_x in range(min(x + 1, columns), columns):
-                view_distance += 1
+        if tree_bottom > max_bottom:
+            visible_trees[upper][y] = True
+            max_bottom = tree_bottom
 
-                if rotated_trees[y][view_x] >= tree:
-                    break
+        if tree_left > max_left:
+            visible_trees[y][lower] = True
+            max_left = tree_left
 
-            rotated_scenic_trees[y][x] *= view_distance
+        # View distance
+        view_distance_top = view_distance_right = view_distance_bottom = view_distance_left = 0
+        view_distance_top_hit = view_distance_right_hit = view_distance_bottom_hit = view_distance_left_hit = False
+        for lower_view in range(lower + 1, columns):
+            upper_view = -1 - lower_view
 
-    trees = rotated_trees
-    visible_trees = rotated_visible_trees
-    scenic_trees = rotated_scenic_trees
+            view_distance_top += 1 if not view_distance_top_hit else 0
+            view_distance_right += 1 if not view_distance_right_hit else 0
+            view_distance_bottom += 1 if not view_distance_bottom_hit else 0
+            view_distance_left += 1 if not view_distance_left_hit else 0
+
+            view_distance_top_hit |= (trees[lower_view][y] >= tree_top)
+            view_distance_right_hit |= (trees[y][upper_view] >= tree_right)
+            view_distance_bottom_hit |= (trees[upper_view][y] >= tree_bottom)
+            view_distance_left_hit |= (trees[y][lower_view] >= tree_left)
+
+        scenic_trees[lower][y] *= view_distance_top
+        scenic_trees[y][upper] *= view_distance_right
+        scenic_trees[upper][y] *= view_distance_bottom
+        scenic_trees[y][lower] *= view_distance_left
 
 print(f'''Number of visible trees: {sum([sum(row) for row in visible_trees])}''')
 print(f'''Maximum scenic score: {max([max(row) for row in scenic_trees])}''')
