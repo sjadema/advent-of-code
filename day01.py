0cc61e85337fab8cba087aeeb3ee1c5a1c1ee35b
Sjoerd Adema <sjoerd@gitlabhost.com>
2023-12-01 17:24:50 +0100

    Day 1 part II

--- day01.py
+++ day01.py
@@ -5,10 +5,7 @@ with open('assets/day01.txt', 'r') as file:
 
 calibrations_values = []
 for line in lines:
-    value = re.sub('\D', '', line)
-    if '' == value:
-        continue
-
+    value = re.sub(r'\D', '', line)
     calibrations_values.append(int(value[0] + value[-1]))
 
 print(f'''Sum of calibration values: {sum(calibrations_values)}''')
