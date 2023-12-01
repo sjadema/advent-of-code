695ec13d6fea872adf17c5b8358bf37c34729e5d
Sjoerd Adema <sjoerd@gitlabhost.com>
2023-12-01 16:03:45 +0100

    Day 1 part I

--- day01.py
+++ day01.py
@@ -1,14 +1,11 @@
+import re
+
 with open('assets/day01.txt', 'r') as file:
-    calories = [line for line in file.read().splitlines()]
+    lines = [line for line in file.read().splitlines()]
 
-calories_per_elf = ' '.join(calories).split('  ')
+calibrations_values = []
+for line in lines:
+    value = re.sub(r'\D', '', line)
+    calibrations_values.append(int(value[0] + value[-1]))
 
-total_calories_per_elf = []
-for elf in calories_per_elf:
-    total_calories_per_elf.append(sum([int(calorie) for calorie in elf.split(' ')]))
-
-print(f'''Maximum total amount of calories: {max(total_calories_per_elf)}''')
-
-ordered_calories = sorted(total_calories_per_elf, reverse=True)
-
-print(f'''Top 3 of calories: {sum(ordered_calories[0:3])}''')
+print(f'''Sum of calibration values: {sum(calibrations_values)}''')
