1744ba6945b8ba6998ce123224239822971e5fd0
Sjoerd Adema <sjadema@gmail.com>
2023-12-04 13:50:45 +0100

    Day 4, part II

--- day04.py
+++ day04.py
@@ -3,12 +3,11 @@ import re
 with open('assets/day04.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
 
-cards = []
+cards = {}
 for line in lines:
     match = re.search(r'^Card\s+(?P<card>\d+):(?P<winning_numbers>[^|]+)\|(?P<present_numbers>.+)$', line)
 
     card = {
-        'card': int(match.group('card')),
         'winning_numbers': [
             int(number) for number in match.group('winning_numbers').strip().replace('  ', ' ').split(' ')
         ],
@@ -17,10 +16,10 @@ for line in lines:
         ],
     }
 
-    cards.append(card)
+    cards[int(match.group('card'))] = card
 
 scores = []
-for card in cards:
+for card in cards.values():
     numbers = set(card['winning_numbers']).intersection(set(card['present_numbers']))
 
     score = len(numbers)
@@ -28,3 +27,15 @@ for card in cards:
         scores.append(2 ** (score - 1))
 
 print(f'''Sum of all scores: {sum(scores)}''')
+
+earned_cards = {card: 1 for card in cards.keys()}
+for i in earned_cards.keys():
+    card = cards[i]
+    numbers = set(card['winning_numbers']).intersection(set(card['present_numbers']))
+    score = len(numbers)
+
+    for j in range(0, earned_cards[i]):
+        for k in range(i + 1, i + score + 1):
+            earned_cards[k] += 1
+
+print(f'''Sum of all cards: {sum(earned_cards.values())}''')
