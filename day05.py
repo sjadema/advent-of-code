fabbadb6c83cd26dcc5e54bae6ff72be39922ac6
Sjoerd Adema <sjadema@gmail.com>
2023-12-06 01:42:24 +0100

    Day 5, part II

--- day05.py
+++ day05.py
@@ -1,5 +1,7 @@
-import operator
-from typing import Optional
+import sys
+import time
+
+start = time.time()
 
 with open('assets/day05.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
@@ -7,13 +9,13 @@ with open('assets/day05.txt', 'r') as file:
 seeds = [int(seed) for seed in lines[0].split(':')[1].strip().split(' ')]
 
 maps = {
-    'seed-to-soil': set(),
-    'soil-to-fertilizer': set(),
-    'fertilizer-to-water': set(),
-    'water-to-light': set(),
-    'light-to-temperature': set(),
-    'temperature-to-humidity': set(),
-    'humidity-to-location': set(),
+    'seed-to-soil': [],
+    'soil-to-fertilizer': [],
+    'fertilizer-to-water': [],
+    'water-to-light': [],
+    'light-to-temperature': [],
+    'temperature-to-humidity': [],
+    'humidity-to-location': [],
 }
 
 i, header = 1, -1
@@ -25,31 +27,67 @@ while i < len(lines):
         continue
 
     sections = list(maps.keys())
-    maps[sections[header]].add(tuple([int(i) for i in lines[i].split(' ')]))
+    maps[sections[header]].append(tuple([int(i) for i in lines[i].split(' ')]))
 
     i += 1
 
-
-def get_rule_value(seed: int, rule: tuple) -> Optional[int]:
-    result = operator.and_(operator.ge(seed, rule[1]), operator.lt(seed, rule[1] + rule[2]))
-
-    return seed + rule[0] - rule[1] if result else None
+for rules in maps.values():
+    rules.sort(key=lambda x: x[1])
 
 
-def get_value(seed: int, rules: set) -> int:
-    for rule in rules:
-        value = get_rule_value(seed, rule)
-        if type(value) is int:
-            return value
+def get_location(seed: int, rule: tuple[int]) -> int:
+    if rule[1] <= seed < rule[1] + rule[2]:
+        return seed + rule[0] - rule[1]
 
     return seed
 
 
-locations = set()
+min_location = sys.maxsize
 for seed in seeds:
     for rules in maps.values():
-        seed = get_value(seed, rules)
+        for rule in rules:
+            location = get_location(seed, rule)
+            if location != seed:
+                seed = location
+                break
 
-    locations.add(seed)
+    min_location = min(min_location, seed)
 
-print(f'''Minimum location: {min(locations)}''')
+print(f'''Minimum location: {min_location}''')
+
+seed_ranges = []
+for i in range(0, len(seeds), 2):
+    seed_ranges.append((seeds[i], seeds[i] + seeds[i + 1]))
+
+
+def get_seed(location: int, rule: tuple[int]) -> int:
+    seed = location + rule[1] - rule[0]
+    if rule[1] <= seed < rule[1] + rule[2]:
+        return seed
+
+    return location
+
+
+min_location = 1
+found = False
+while not found:
+    location = min_location
+    for rules in reversed(maps.values()):
+        for rule in rules:
+            seed = get_seed(location, rule)
+            if seed != location:
+                location = seed
+                break
+
+    for seed_range in seed_ranges:
+        if seed_range[0] <= location < seed_range[1]:
+            found = True
+            break
+
+    if min_location % 1000000 == 0:
+        print(f'Location done: {min_location}')
+
+    min_location += 1
+
+print(f'''Minimum location: {min_location - 1}''')
+print(f'Done in {time.time() - start}')
