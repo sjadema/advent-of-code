3e7572b842fb3fee3f2c530637d16725a6a5a986
Sjoerd Adema <sjadema@gmail.com>
2023-12-10 02:13:43 +0100

    Day 9, part II

--- day09.py
+++ day09.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 with open('assets/day09.txt', 'r') as file:
     lines = [[int(value) for value in line.split(' ')] for line in file.read().splitlines()]
 
@@ -5,24 +7,18 @@ collections = []
 for line in lines:
     collection = [[value for value in line]]
 
-    while len(set(values := [value for value in collection[-1]]).difference({0})) != 0:
+    while len(set(values := collection[-1]).difference({0})) != 0:
         new_values = []
         for i in range(1, len(values)):
             new_values.append(values[i] - values[i - 1])
 
         collection.append(new_values)
 
-    collections.append(collection)
+    collection.reverse()
 
-for collection in collections:
-    differences = collection[::-1]
-    differences[0].append(0)
+    historic_value = reduce(lambda a, b: b - a, [differences[0] for differences in collection])
+    future_value = sum([differences[-1] for differences in collection])
+    collections.append([historic_value, future_value])
 
-    for i in range(1, len(differences)):
-        difference = differences[i]
-        difference.append(difference[-1] + differences[i - 1][-1])
-
-all_differences = [differences[0][-1] for differences in collections]
-print(f'Sum of added differences: {sum(all_differences)}')
-
-
+print(f'Sum of future values: {sum([collection[1] for collection in collections])}')
+print(f'Sum of historic values: {sum([collection[0] for collection in collections])}')
