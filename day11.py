628b51ab46de3a2adae0a3261ec507f37775eebd
Sjoerd Adema <sjadema@gmail.com>
2023-12-13 00:46:53 +0100

    Day 11, part II

--- day11.py
+++ day11.py
@@ -1,48 +1,47 @@
 import operator
+from typing import List, Dict
 
 with open('assets/day11.txt', 'r') as file:
-    lines = [line for line in file.read().splitlines()]
+    lines = [list(line) for line in file.read().splitlines()]
 
-universe = []
 
-# Expand horizontal
-y = 0
-while y < len(lines):
-    line = lines[y]
+def inflate_galaxies(scale: int) -> List[tuple]:
+    if scale < 1:
+        raise Exception("Can't deflate galaxies!")
 
-    universe.append(line)
-    if set(list(line)) == {'.'}:
-        universe.append(line)
+    scale -= 1
 
-    y += 1
+    def inflate_axis(axis: str, galaxies: List[List[str]], scale: int) -> Dict[tuple[int, int], int]:
+        inflation_map, offset = {}, 0
 
-# Expand vertical
-x, offset = 0, 0
-while x < len(lines[0]):
-    line = [line[x] for line in lines]
+        for i in range(len(galaxies)):
+            if set(list(galaxies[i])) == {'.'}:
+                offset += 1
+                continue
 
-    if set(line) == {'.'}:
-        for i in range(len(universe)):
-            row = list(universe[i])
-            row.insert(x + offset, '.')
+            for j in range(len(galaxies[i])):
+                if galaxies[i][j] == '#':
+                    point = (j, i) if axis == 'y' else (i, j)
+                    inflation_map[point] = i + offset * scale
 
-            universe[i] = ''.join(row)
+        return inflation_map
 
-        offset += 1
+    y_axis = inflate_axis('y', lines, scale)
+    transposed = [[lines[y][x] for y in range(len(lines))] for x in range(len(lines[0]))]
+    x_axis = inflate_axis('x', transposed, scale)
 
-    x += 1
+    return [(x_axis[point], y_axis[point]) for point in x_axis.keys()]
 
-galaxies = []
-for y in range(height := len(universe)):
-    row = universe[height - y - 1]
-    for x in range(len(row)):
-        if row[x] == '#':
-            galaxies.append((x, y))
 
-distances = 0
-for i in range(len(galaxies)):
-    for j in range(i, len(galaxies)):
-        distance = tuple(map(operator.sub, galaxies[i], galaxies[j]))
-        distances += abs(distance[0]) + abs(distance[1])
+def calculate_distances(galaxies: List[tuple]) -> int:
+    distances = 0
+    for i in range(len(galaxies)):
+        for j in range(i, len(galaxies)):
+            distance = tuple(map(operator.sub, galaxies[i], galaxies[j]))
+            distances += abs(distance[0]) + abs(distance[1])
 
-print(f'Sum of shortest paths: {distances}')
+    return distances
+
+
+print(f'Sum of shortest paths with 2 inflation: {calculate_distances(inflate_galaxies(2))}')
+print(f'Sum of shortest paths with 1000000 inflation: {calculate_distances(inflate_galaxies(1000000))}')
