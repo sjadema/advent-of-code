bb6e2b47cfdb0b2df400f2a77e4b527e26148b36
Sjoerd Adema <sjadema@gmail.com>
2023-12-13 00:58:29 +0100

    Day 8, part II

--- day08.py
+++ day08.py
@@ -22,3 +22,44 @@ while name != 'ZZZ':
     step += 1
 
 print(f'Node ZZZ reached in {step} steps')
+
+current_names = [name for name in nodes.keys() if name[-1] == 'A']
+target_names = [name for name in nodes.keys() if name[-1] == 'Z']
+
+intervals = [0] * 6
+step = 0
+while not all(intervals):
+    direction = directions[step % len(directions)]
+
+    current_nodes = [nodes[current_name] for current_name in current_names]
+    current_names = []
+
+    for current_node in current_nodes:
+        name = current_node[direction_positions.index(direction)]
+
+        current_names.append(name)
+
+    step += 1
+    for i in range(len(current_names)):
+        current_name = current_names[i]
+        if current_name[-1] == 'Z':
+            intervals[i] = step
+
+
+def lcm(a: int, b: int) -> int:
+    largest, smallest = max(a, b), min(a, b)
+
+    def gcd(largest: int, smallest: int) -> int:
+        if smallest == 0:
+            return largest
+
+        return gcd(smallest, largest % smallest)
+
+    return smallest * (largest // gcd(largest, smallest))
+
+
+k = 1
+for interval in intervals:
+    k = lcm(k, interval)
+
+print(f'Nodes **Z reached simultaneously in {k} steps')
