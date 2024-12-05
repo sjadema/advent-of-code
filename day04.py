413bad3d4e8c89dd88597ed1761f4e7956aea460
Sjoerd Adema <sjoerd@triz.nu>
2024-12-05 23:02:03 +0100

    Day 4, part II

--- day04.py
+++ day04.py
@@ -24,6 +24,16 @@ for y in range(0, len(puzzle)):
         characters[(x, y)] = puzzle[y][x]
 
 
+def get_next_coordinate(coordinate: Coordinate, direction: Coordinate) -> Coordinate:
+    return tuple(map(operator.add, coordinate, direction))
+
+
+def get_coordinate_difference(a: Coordinate, b: Coordinate) -> Coordinate:
+    difference = tuple(map(operator.sub, a, b))
+
+    return (abs(difference[0]), abs(difference[1]))
+
+
 def get_character_at(coordinate: Coordinate) -> str:
     try:
         return characters[coordinate]
@@ -31,12 +41,12 @@ def get_character_at(coordinate: Coordinate) -> str:
         return ''
 
 
-def search_word(start: Coordinate, direction: str) -> str:
+def search_word(start: Coordinate, direction: str, length: int) -> str:
     word = get_character_at(start)
 
     coordinate = start
-    for i in range(0, 3):
-        coordinate = tuple(map(operator.add, coordinate, directions[direction]))
+    for i in range(0, length - 1):
+        coordinate = get_next_coordinate(coordinate, directions[direction])
         word += get_character_at(coordinate)
 
     return word
@@ -44,10 +54,38 @@ def search_word(start: Coordinate, direction: str) -> str:
 
 found_words = 0
 for coordinate in characters:
-    for direction in directions.keys():
-        word = search_word(coordinate, direction)
+    if get_character_at(coordinate) != 'X':
+        continue
 
+    for direction in directions.keys():
+        word = search_word(coordinate, direction, 4)
         if word == 'XMAS':
             found_words += 1
 
 print(f'Number of times XMAS in puzzle: {found_words}')
+
+found_coordinates = []
+for coordinate in characters:
+    if get_character_at(coordinate) != 'M':
+        continue
+
+    for direction in directions.keys():
+        word = search_word(coordinate, direction, 3)
+
+        if word == 'MAS':
+            coordinates = [coordinate]
+            for i in range(0, 2):
+                coordinates.append(get_next_coordinate(coordinates[-1], directions[direction]))
+
+            found_coordinates.append(coordinates)
+
+found_crosses = 0
+for i in range(0, len(found_coordinates) - 1):
+    for j in range(i + 1, len(found_coordinates)):
+        a, b = found_coordinates[i], found_coordinates[j]
+        if a[1] == b[1]:
+            difference = get_coordinate_difference(a[0], b[0])
+            if difference in [(2, 0), (0, 2)]:
+                found_crosses += 1
+
+print(f'Number of times X-MAS in puzzle: {found_crosses}')
