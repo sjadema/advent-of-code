aa17513aaaf557b77d7e6a47b978e86c39c96039
Sjoerd Adema <sjoerd@triz.nu>
2024-12-07 14:06:47 +0100

    Day 7, part II

--- day07.py
+++ day07.py
@@ -1,5 +1,6 @@
 import operator
 import re
+from typing import List
 
 with open('assets/day07.txt', 'r') as file:
     lines = [line for line in file.read().splitlines()]
@@ -14,26 +15,71 @@ for line in lines:
 
     calculations.append(calculation)
 
-operations = [operator.add, operator.mul]
 
-valid_results = []
+def __to_binary(decimal: int, size: int) -> str:
+    return f'{decimal:b}'.rjust(size, '0')
+
+
+def __to_ternary(decimal: int, size: int) -> str:
+    def __to_raw_ternary(n: int) -> str:
+        if n == 0:
+            return '0'
+
+        remainders = []
+        while n:
+            n, r = divmod(n, 3)
+            remainders.append(str(r))
+
+        return ''.join(reversed(remainders))
+
+    return __to_raw_ternary(decimal).rjust(size, '0')
+
+
+def __concat_int(a: int, b: int) -> int:
+    return int(str(a) + str(b))
+
+
+operations = [operator.add, operator.mul, __concat_int]
+
+
+def __has_valid_result(r: int, n_s: List[int], o_s: List[str]) -> bool:
+    for o in o_s:
+        p_r = n_s[0]
+
+        for i in range(0, len(o)):
+            operation = operations[int(o[i])]
+            p_r = operation(p_r, n_s[i + 1])
+
+        if p_r == r:
+            return True
+
+    return False
+
+
+valid_results_binary = []
+valid_results_ternary = []
+
 for calculation in calculations:
     result = calculation['result']
     numbers = calculation['numbers']
     number_of_operations = len(numbers) - 1
 
-    possible_operations = []
-    for i in range(0, 2 ** number_of_operations):
-        possible_operations.append(f'{i:b}'.rjust(number_of_operations, '0'))
+    binary_operations = []
+    for b in range(0, 2 ** number_of_operations):
+        binary_operations.append(__to_binary(b, number_of_operations))
 
-    for possible_operation in possible_operations:
-        possible_result = numbers[0]
-        for i in range(0, len(possible_operation)):
-            operation = operations[int(possible_operation[i])]
-            possible_result = operation(possible_result, numbers[i + 1])
+    if __has_valid_result(result, numbers, binary_operations):
+        valid_results_binary.append(result)
+        continue
 
-        if possible_result == result:
-            valid_results.append(result)
-            break
+    ternary_operations = []
+    for t in range(0, 3 ** number_of_operations):
+        ternary_operations.append(__to_ternary(t, number_of_operations))
 
-print(f'Sum of valid results: {sum(valid_results)}')
+    if __has_valid_result(result, numbers, ternary_operations):
+        valid_results_ternary.append(result)
+        continue
+
+print(f'Sum of valid binary results: {sum(valid_results_binary)}')
+print(f'Sum of valid ternary results: {sum(valid_results_ternary)}')
+print(f'Sum of all valid results: {sum(valid_results_binary) + sum(valid_results_ternary)}')
